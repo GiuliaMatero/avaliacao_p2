c0dc03198300c684b04497dc5fe658be2711e7f3
GiuliaMatero <giulia.diaz@fatec.sp.gov.br>
2024-06-13 21:33:55 -0300

    feat(P2-pdmn): estilizando a interface

--- P2/app/index.tsx
+++ P2/app/index.tsx
@@ -1,51 +1,28 @@
 import { useState } from 'react';
 import { TheCatAPI } from "@thatapicompany/thecatapi"
 import { StyleSheet, Text, View, Pressable, FlatList, Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-type Gatos = {
-  id: string;
-  url: string;
-  limit?: number;
-}
+
 const { API_KEY } = process.env;
 
 export default function App() {
-  const theCatAPI = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&limit=5`
-  const [gatos, setGatos] = useState<Gatos[]>([]);
-
-  const gerarGato = async () => {
-
-    const fotos = await fetch(theCatAPI);
-    const data = await fotos.json();
-    const gatos = data.slice(0, 5);
-
-    setGatos(gatos);
-
-    console.log(gatos);
-    console.log(theCatAPI)
-    
-  }
+  
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Gerador de fotos de gatos</Text>
+      <Text style={styles.titulo}>Carrossel de gatinhos fofos :)</Text>
       <Pressable
         style={styles.button}
-        onPress={gerarGato}
       >
-        <Text>Gerar</Text>
+        <Text style ={styles.text}>Oi, gatinhos!</Text>
       </Pressable>
 
       <FlatList
-        keyExtractor={(item) => item.id!}
-        data={gatos}
         style={styles.list}
         renderItem={gatos => (
         <View>
-          <Image
-            source={{ uri: gatos.item.url }}
-            style={styles.imagem}
-          />
+
         </View>
         )}
       >
@@ -57,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'Linen',
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 40,
@@ -65,15 +42,19 @@ const styles = StyleSheet.create({
 
   titulo: {
     fontSize: 20,
-    marginBottom: 20,
+    marginBottom: 3,
+    color: 'indigo',
+    fontFamily: 'PathwayGothicOne',
+    borderBottomWidth: 2,
+    borderBottomColor: '#9370DB',
   },
 
   button: {
-    borderColor: 'black',
-    borderWidth: 3,
+    borderColor: '#9370DB',
+    borderWidth: 2,
     borderRadius: 4,
     marginTop: 15,
-    width: 80,
+    width: 100,
     height: 35,
     justifyContent: 'center',
     alignItems: 'center',
@@ -82,8 +63,8 @@ const styles = StyleSheet.create({
   },
 
   list: {
-    borderWidth: 1,
-    borderColor: 'black',
+    borderWidth: 2,
+    borderColor: '#9370DB',
     borderRadius: 4,
     width: '80%',
     padding: 10,
@@ -93,6 +74,12 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 200,
     marginBottom: 10,
+  },
+  
+  text: {
+    color: 'indigo',
+    fontFamily: 'PathwayGothicOne',
+
   }
 
 });
